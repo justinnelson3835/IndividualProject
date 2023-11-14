95dce65c24eecd6fa86b6045c5ccb933a5f8b7c5
justinnelson3835 <justinnelson3835@gmail.com>
2023-11-14 10:23:23 -0700

    Database Set Up

--- src/model.js
+++ src/model.js
@@ -2,7 +2,7 @@ import { DataTypes, Model } from 'sequelize';
 import util from 'util';
 import connectToDB from './db.js';
 
-export const db = await connectToDB('postgresql:///ratings');
+export const db = await connectToDB('postgresql:///trailertrash');
 
 export class User extends Model {
   [util.inspect.custom]() {
